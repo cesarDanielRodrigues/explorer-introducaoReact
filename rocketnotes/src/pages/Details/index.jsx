372de73c43ec97ddb7eb1fff9624722737d9e33c
César Daniel dos Santos Rodrigues <cesar.daniel.rodrigues@gmail.com>
2024-10-22 19:44:44 -0300

    CSS in JS

---
+++ rocketnotes/src/pages/Details/index.jsx
@@ -0,0 +1,13 @@
+import { Container } from "./style"
+
+
+export function Details(){
+
+  return(
+    <Container>
+      <h1>Hello World1</h1>
+      <span>César Daniel</span>
+    </Container>
+  )
+}
+

c7673e5ad45fe156470acc548c7b3a4f153b7196
César Daniel dos Santos Rodrigues <cesar.daniel.rodrigues@gmail.com>
2024-10-22 20:10:58 -0300

    Tema

--- rocketnotes/src/main.jsx
+++ rocketnotes/src/main.jsx
@@ -1,9 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import {Details} from './pages/Details'
+import {ThemeProvider} from'styled-components';
+import './styles/theme'
+
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Details />
+    <ThemeProvider theme={theme}>
+      <Details />
+    </ThemeProvider>
   </StrictMode>,
 )

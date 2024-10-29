0f553403cdf92977a49ac67d3b5f2d7670aedf6a
César Daniel dos Santos Rodrigues <cesar.daniel.rodrigues@gmail.com>
2024-10-29 17:30:49 -0300

    Componente ButtonText

--- rocketnotes/src/pages/Details/index.jsx
+++ rocketnotes/src/pages/Details/index.jsx
@@ -1,5 +1,6 @@
 import { Container, Links } from "./style"
 import { Header } from "../../components/Header"
+import { ButtonText } from "../../components/ButtonText"
 import { Section } from "../../components/Section"
 import { Tag } from "../../components/tag"
 import { Button } from "../../components/Button"
@@ -8,22 +9,22 @@ export function Details() {
   return (
     <Container>
       <Header />
+      <ButtonText title="Excluir a Nota"/>
       <Section title="Link úteis">
         <Links>
-            <li>
-              <a href="#">Link 1</a>
-            </li>
-            <li>
-              <a href="#">Link 1</a>
-            </li>
+          <li>
+            <a href="#">Link 1</a>
+          </li>
+          <li>
+            <a href="#">Link 1</a>
+          </li>
         </Links>
       </Section>
 
       <Section title="Marcadores">
-        <Tag title="express"/>
-        <Tag title="nodejs"/>
+        <Tag title="express" />
+        <Tag title="nodejs" />
       </Section>
-
 
       <Button title="Voltar" />
     </Container>

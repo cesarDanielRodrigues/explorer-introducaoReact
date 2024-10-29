77a3a15048e85e48266e788033932183b48f5745
César Daniel dos Santos Rodrigues <cesar.daniel.rodrigues@gmail.com>
2024-10-29 10:51:47 -0300

    Componentes Header

--- rocketnotes/src/pages/Details/index.jsx
+++ rocketnotes/src/pages/Details/index.jsx
@@ -1,14 +1,12 @@
 import { Container } from "./style"
+import { Header } from "../../components/Header"
 import { Button } from "../../components/Button"
 
 export function Details(){
 
   return(
     <Container>
-      <h1>Hello World</h1>
-      <span>César Daniel</span>
-      <Button title="Entrar" loading/>
-      <Button title="Cadastrar"/>
+      <Header/>
       <Button title="Voltar"/>
     </Container>
   )

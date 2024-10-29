d18132d49fe92478fdf62b9b05a4f9ec03e216a3
César Daniel dos Santos Rodrigues <cesar.daniel.rodrigues@gmail.com>
2024-10-29 11:08:29 -0300

    Imagem do usuário

--- rocketnotes/src/components/Header/index.jsx
+++ rocketnotes/src/components/Header/index.jsx
@@ -1,9 +1,16 @@
-import { Container } from "./styles";
+import { Container, Profile } from "./styles";
 
 export function Header(){
     return(
         <Container>
-            
+            <Profile>
+                <img src="https://github.com/cesarDanielRodrigues.png" alt="Foto do usuário"/>
+
+                <div>
+                    <span>Bem-vindo,</span>
+                    <strong>César Daniel</strong>
+                </div>
+            </Profile>
         </Container>
     )
 }

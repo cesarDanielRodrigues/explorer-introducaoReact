7f4aaeacd0c1a809202d5850fd49a8219bf4c917
César Daniel dos Santos Rodrigues <cesar.daniel.rodrigues@gmail.com>
2024-10-29 11:25:23 -0300

    Ícones

--- rocketnotes/src/components/Header/index.jsx
+++ rocketnotes/src/components/Header/index.jsx
@@ -1,16 +1,20 @@
-import { Container, Profile } from "./styles";
+import { RiShutDownLine } from "react-icons/ri"
+import { Container, Profile, Logout} from "./styles"
 
-export function Header(){
-    return(
-        <Container>
-            <Profile>
-                <img src="https://github.com/cesarDanielRodrigues.png" alt="Foto do usuário"/>
+export function Header() {
+  return (
+    <Container>
+      <Profile>
+        <img src="https://github.com/cesarDanielRodrigues.png" alt="Foto do usuário" />
 
-                <div>
-                    <span>Bem-vindo,</span>
-                    <strong>César Daniel</strong>
-                </div>
-            </Profile>
-        </Container>
-    )
+        <div>
+          <span>Bem-vindo,</span>
+          <strong>César Daniel</strong>
+        </div>
+      </Profile>
+      <Logout>
+        <RiShutDownLine />
+      </Logout>
+    </Container>
+  )
 }

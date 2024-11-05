f55860512b0e5a6568c3aa190fc324bda765d587
César Daniel dos Santos Rodrigues <cesar.daniel.rodrigues@gmail.com>
2024-11-05 09:18:22 -0300

    Filtros

--- rocketnotes/src/pages/Home/index.jsx
+++ rocketnotes/src/pages/Home/index.jsx
@@ -1,17 +1,28 @@
-import { Container, Brand, Menu, Search, Content, NewNote } from "./styles";
-import { Header } from "../../components/Header";
+import { Container, Brand, Menu, Search, Content, NewNote } from "./styles"
+import { Header } from "../../components/Header"
+import { ButtonText } from "../../components/ButtonText"
 
-export function Home(){
-    return(
-        <Container>
-            <Brand>
-                <h1>RocketNote</h1>
-            </Brand>
-            <Header></Header>
-            <Menu></Menu>
-            <Search></Search>
-            <Content></Content>
-            <NewNote></NewNote>
-        </Container>
-    )
+export function Home() {
+  return (
+    <Container>
+      <Brand>
+        <h1>RocketNote</h1>
+      </Brand>
+      <Header></Header>
+      <Menu>
+        <li>
+          <ButtonText title={"Todos"} />
+        </li>
+        <li>
+          <ButtonText title={"ReactJS"} />
+        </li>
+        <li>
+          <ButtonText title={"NodeJS"} />
+        </li>
+      </Menu>
+      <Search></Search>
+      <Content></Content>
+      <NewNote></NewNote>
+    </Container>
+  )
 }

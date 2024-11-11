ab3f5a87237341783a0ba25ec2d7f760feb1e400
César Daniel dos Santos Rodrigues <cesar.daniel.rodrigues@gmail.com>
2024-11-11 17:26:49 -0300

    Formulário da SignIn

--- rocketnotes/src/pages/SignIn/index.jsx
+++ rocketnotes/src/pages/SignIn/index.jsx
@@ -1,9 +1,25 @@
-import { Container } from "./styles";
+import { Container,Form } from "./styles"
+import { Input } from "../../components/Input"
+import { Button } from "../../components/Button"
 
-export function SignIn(){
-    return(
-        <Container>
-            
-        </Container>
-    )
+import { FiLogIn, FiMail, FiLock } from "react-icons/fi"
+
+export function SignIn() {
+  return (
+    <Container>
+      <Form>
+        <h1>Rocket Notes</h1>
+        <p>Aplicação para salvar e gerenciar seus links úteis.</p>
+        <h2>Faça seu login</h2>
+
+        <Input placeholder={"E-mail"} type={"text"} icon={FiMail} />
+        <Input placeholder={"Senha"} type={"password"} icon={FiLock} />
+        <Button title={"Enviar"} />
+
+        <a href="#">
+            Criar Conta
+        </a>
+      </Form>
+    </Container>
+  )
 }

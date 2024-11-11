54facdb4c478413f5312206c4c8080d8b2449ffd
César Daniel dos Santos Rodrigues <cesar.daniel.rodrigues@gmail.com>
2024-11-11 15:36:19 -0300

    Componentes de Notas

--- rocketnotes/src/pages/Home/index.jsx
+++ rocketnotes/src/pages/Home/index.jsx
@@ -1,9 +1,12 @@
-import { FiPlus,FiSearch } from 'react-icons/fi'
+import { FiPlus, FiSearch } from "react-icons/fi"
 
 import { Container, Brand, Menu, Search, Content, NewNote } from "./styles"
 import { Header } from "../../components/Header"
 import { ButtonText } from "../../components/ButtonText"
-import { Input } from '../../components/input'
+import { Input } from "../../components/input"
+import { Note } from "../../components/Note"
+import { Section } from "../../components/Section"
+import { Tag } from "../../components/tag"
 
 export function Home() {
   return (
@@ -24,11 +27,23 @@ export function Home() {
         </li>
       </Menu>
       <Search>
-        <Input  placeholder="Pesquisar pelo título" />
+        <Input placeholder="Pesquisar pelo título" />
       </Search>
-      <Content></Content>
+      <Content>
+        <Section title={"Minha notas"}>
+          <Note
+            data={{
+              title: "React",
+              tags: [
+                { id: "1", name: "react" },
+                { id: "2", name: "rockeseat" },
+              ],
+            }}
+          />
+        </Section>
+      </Content>
       <NewNote>
-        <FiPlus/>
+        <FiPlus />
         Nova Nota
       </NewNote>
     </Container>

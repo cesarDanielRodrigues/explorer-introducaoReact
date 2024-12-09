59c7c94ae1e1c2faace33401a8f7f3e81ecc188d
César Daniel dos Santos Rodrigues <cesar.daniel.rodrigues@gmail.com>
2024-12-09 17:51:30 -0300

    Header da New

--- rocketnotes/src/pages/New/index.jsx
+++ rocketnotes/src/pages/New/index.jsx
@@ -1,10 +1,20 @@
-import {Container} from './styles'
-import {Header} from '../../components/Header'
+import { Container, Form } from "./styles"
+import { Header } from "../../components/Header"
+import { Input } from "../../components/Input"
 
-export function New(){
-    return (
-        <Container>
-            <Header/>
-        </Container>
-    )
+export function New() {
+  return (
+    <Container>
+      <Header />
+      <main>
+        <Form>
+          <header>
+            <h1>Criar nota</h1>
+            <a href="/">voltar</a>
+          </header>
+          <Input placeholder="Título" />
+        </Form>
+      </main>
+    </Container>
+  )
 }
--- rocketnotes/src/pages/New/styles.js
+++ rocketnotes/src/pages/New/styles.js
@@ -12,3 +12,21 @@ export const Container = styled.div`
     "content" ;
 
 `
+export const Form = styled.form`
+    max-width: 550px;
+    margin: 38px auto;
+
+    > header{
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        margin-bottom: 36px;
+
+        > a{
+            color:${
+                ({theme})=>theme.COLORS.GRAY_100
+            };
+            font-size: 20px;
+        }
+    }
+`

f023fb488376cb8d4ff1fbbf6d9e1c14a566d38f
César Daniel dos Santos Rodrigues <cesar.daniel.rodrigues@gmail.com>
2024-12-09 07:50:40 -0300

    Profile

--- rocketnotes/src/main.jsx
+++ rocketnotes/src/main.jsx
@@ -1,19 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from "react"
+import { createRoot } from "react-dom/client"
 
-import { SignIn } from './pages/SignIn';
-import { SignUp } from './pages/SignUp';
+import { Profile } from "./pages/Profile"
 
-import {ThemeProvider} from'styled-components';
-import theme from './styles/theme';
+import { ThemeProvider } from "styled-components"
+import theme from "./styles/theme"
 
-import GlobalStyles from './styles/global'
+import GlobalStyles from "./styles/global"
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <ThemeProvider theme={theme}>
-      <GlobalStyles/>
-      <SignUp/>
+      <GlobalStyles />
+      <Profile />
     </ThemeProvider>
-  </StrictMode>,
+  </StrictMode>
 )

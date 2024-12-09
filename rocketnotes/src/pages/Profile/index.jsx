c150b27947b11ed104cb4ae692333de6240ec4d3
César Daniel dos Santos Rodrigues <cesar.daniel.rodrigues@gmail.com>
2024-12-09 09:49:19 -0300

    Avatar do usuário

--- rocketnotes/src/pages/Profile/index.jsx
+++ rocketnotes/src/pages/Profile/index.jsx
@@ -1,5 +1,5 @@
-import { Container, Form } from "./styles"
-import { FiArrowLeft, FiUser, FiMail, FiLock } from "react-icons/fi"
+import { Container, Form, Avatar } from "./styles"
+import { FiArrowLeft, FiUser, FiMail, FiLock, FiCamera } from "react-icons/fi"
 import { Input } from "../../components/Input"
 import { Button } from "../../components/Button"
 
@@ -12,6 +12,13 @@ export function Profile() {
         </a>
       </header>
       <Form>
+        <Avatar>
+          <img src="https://github.com/cesarDanielRodrigues.png" alt="Foto do GitHub" />
+          <label htmlFor="avatar">
+            <FiCamera/>
+            <input type="file" id="avatar"/>
+          </label>
+        </Avatar>
         <Input placeholder="Nome" type="text" icon={FiUser} />
         <Input placeholder="Email" type="text" icon={FiMail} />
         <Input placeholder="Senha atual" type="password" icon={FiLock} />
